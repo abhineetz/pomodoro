4c13e5b76e04713c5dbb306d9ba5c6df195720f0
abhineetz <abhineetz@gmail.com>
2021-04-29 00:06:09 +0530

    beginning

---
+++ README.md
@@ -0,0 +1,6 @@
+
+h2. Run program 
+
+abhin@DESKTOP-VEME1O5 MINGW64 ~/repos/pomodoro (master)
+$ mvn spring-boot:run
+
--- src/main/java/in/abhineetkapil/App.java
+++ src/main/java/in/abhineetkapil/App.java
@@ -1,13 +1,71 @@
 package in.abhineetkapil;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@SpringBootApplication
+public class App extends JFrame {
+	
+	public App () {
+  initUI();
+    }
+
+    private void initUI() {
+
+        var quitButton = new JButton("Quit");
+
+        quitButton.addActionListener((ActionEvent event) -> {
+			try{
+			Thread.sleep(2000);
+			} catch (InterruptedException e){
+			}
+		  System.exit(0);
+        });
+
+        createLayout(quitButton);
+
+        setTitle("Quit button");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+	private void createLayout(JComponent... arg) {
+
+        var pane = getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
+
+    public static void main(String[] args) {
+
+        var ctx = new SpringApplicationBuilder(App.class)
+                .headless(false).run(args);
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = ctx.getBean(App.class);
+            ex.setVisible(true);
+        });
     }
 }
