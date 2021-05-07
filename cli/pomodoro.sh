83cc4f17ddd9338144b83a59229be2efad41aca8
abhineetz <abhineetz@gmail.com>
2021-05-07 23:59:03 +0530

    Add notification sound

--- cli/pomodoro.sh
+++ cli/pomodoro.sh
@@ -2,16 +2,20 @@
 
 export MS_OFFICE="/c/Program Files/Microsoft Office/root/Office16/";
 
+currPath="`dirname \"$0\"`";
+
 pomodoro () {
 	"$MS_OFFICE"/EXCEL.exe pomodoro.csv & 
 	sleep "$1";	
 
-	echo "Waking up.. after $1";
+	echo "Waking up.. after $1\r\n";
+
+	cat "$currPath"/resources/beep.txt;
 }
 
 
 
-printf "%b" " Usage: Start a pomodoro timer with a custome time interval\r\n\tIf not agrument is provided, the the default timer duration will be taken as 30 mins.\r\n e.g. ./pomodoro 20m";
+printf "%b" " Usage: Start a pomodoro timer with a custome time interval\r\n\tIf not agrument is provided, the the default timer duration will be taken as 30 mins.\r\n e.g. ./pomodoro 20m\r\n";
 
 if [ "$#" -eq 1 ]; then
 	pomodoro $@;
